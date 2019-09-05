8f261a37ad90107cf96f16ed2e14d21f16d14d03
skuethe <crazycapivara@gmail.com>
2019-09-05 11:51:37 +0200

    Add basemap

---
+++ inst/htmlwidgets/lib/mapbox-gl/get-mapbox-gl.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+wget https://api.tiles.mapbox.com/mapbox-gl-js/v1.0.0/mapbox-gl.css
+wget https://api.tiles.mapbox.com/mapbox-gl-js/v1.0.0/mapbox-gl.js
+
--- javascript/src/cartovl-widget.js
+++ javascript/src/cartovl-widget.js
@@ -1,11 +1,25 @@
 export default function(widgetElement, width, height) {
   var widget = {};
 
+  var map = null;
+
   widget.renderValue = function(widgetData) {
-    widgetElement.innerText = widgetData.message;
+    // widgetElement.innerText = widgetData.message;
+
+    map = makeMap(widgetElement.id);
   };
 
   widget.resize = function(width, height) { };
 
   return widget;
 }
+
+var makeMap = function(elementId) {
+  const map = new mapboxgl.Map({
+    container: elementId,
+    style: carto.basemaps.voyager,
+    center: [0, 30],
+    zoom: 2
+  });
+  return map;
+};
