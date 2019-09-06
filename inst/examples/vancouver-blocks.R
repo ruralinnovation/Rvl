40d5c7ac20f96aaeae19e7444ab246ba4d53dbdc
skuethe <crazycapivara@gmail.com>
2019-09-06 13:21:34 +0200

    Add another example

---
+++ inst/examples/vancouver-blocks.R
@@ -0,0 +1,15 @@
+library(sf)
+library(geojsonio)
+library(cartovl)
+
+data_url <- paste0(
+  "https://raw.githubusercontent.com/uber-common/deck.gl-data/",
+  "master/examples/geojson/vancouver-blocks.json"
+)
+
+blocks <- st_read(data_url) %>%
+  geojson_json()
+
+cartovl() %>%
+  set_view(latitude = 49.254, longitude = -123.13, zoom = 11) %>%
+  add_layer(blocks, list("color: ramp($growth, [red, green, yellow])"))
