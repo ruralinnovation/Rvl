fa8650eb7ec227c837d642251420460762847691
skuethe <crazycapivara@gmail.com>
2019-09-06 13:04:52 +0200

    Refactor

--- R/map-properties.R
+++ R/map-properties.R
@@ -5,9 +5,9 @@
 #' @param zoom zoom
 #' @export
 map_properties <- function(
-                           style = "https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json",
-                           center = c(0, 0),
-                           zoom = 2) {
+  style = "https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json",
+  center = c(0, 0),
+  zoom = 2) {
   list(
     style = style,
     center = center,
