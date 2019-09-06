664f1cf5bd8135eb30b1fbcef03ed33004a8b71f
skuethe <crazycapivara@gmail.com>
2019-09-06 09:57:54 +0200

    Add func to set view

--- R/map-properties.R
+++ R/map-properties.R
@@ -6,3 +6,15 @@ map_properties <- function(style = "https://basemaps.cartocdn.com/gl/dark-matter
     zoom = zoom
   )
 }
+
+#' @export
+set_view <- function(map, longitude, latitude, zoom) {
+  map$x$mapProperties <- modifyList(
+    map$x$mapProperties,
+    list(
+      center = c(longitude, latitude),
+      zoom = zoom
+      )
+    )
+  map
+}
