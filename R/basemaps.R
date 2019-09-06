b2519ae46fcbcb43ca0975602e4aeb5bd3e7a243
skuethe <crazycapivara@gmail.com>
2019-09-06 14:25:45 +0200

    Add func to add carto base maps

--- R/basemaps.R
+++ R/basemaps.R
@@ -8,3 +8,19 @@
 add_mapbox_basemap <- function(map, token = Sys.getenv("MAPBOX_API_TOKEN"), style = "mapbox://styles/mapbox/light-v9") {
   modify_map_parameters(map, style = style, mapboxAccessToken = token)
 }
+
+#' Add a base map from carto to the map widget
+#'
+#' @inheritParams add_mapbox_basemap
+#' @param style map style, one of \code{darkmatter}, \code{positron} or \code{voyager}
+#'
+#' @export
+add_carto_basemap <- function(map, style = "darkmatter") {
+  modify_map_parameters(map, style = CARTO_BASEMAPS[[style]])
+}
+
+CARTO_BASEMAPS <- list(
+  darkmatter = htmlwidgets::JS("carto.basemaps.darkmatter"),
+  positron = htmlwidgets::JS("carto.basemaps.positron"),
+  voyager = htmlwidgets::JS("carto.basemaps.voyager")
+)
