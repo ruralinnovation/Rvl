12fedb98c86d48c0e0346c1d96ff92e7599e87d0
skuethe <crazycapivara@gmail.com>
2019-09-11 18:56:51 +0200

    Remove dead code

--- javascript/src/cartovl-widget.js
+++ javascript/src/cartovl-widget.js
@@ -30,7 +30,6 @@ export default function(widgetElement, width, height) {
     logVersions();
     mapboxgl.accessToken = widgetData.mapProperties.mapboxAccessToken;
     map = _cartoVLWidget.map = makeMap(widgetElement.id, widgetData.mapProperties);
-    // addControls(map, widgetData.controls);
     const layers = _cartoVLWidget.layers = makeLayers(map, widgetData.layers);
 
     // TODO: add layers in 'makeLayers' func
@@ -59,11 +58,3 @@ const makeMap = function(elementId, properties) {
   });
   return map;
 };
-
-/*
-const addControls = function(map, controls) {
-  controls.forEach(control => {
-    map.addControl(new mapboxgl[control.name](control.props), control.position || "top-left");
-  });
-};
-*/
