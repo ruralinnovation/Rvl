5d6f72c4078752b6b768ce076267a8e13cb2f991
skuethe <crazycapivara@gmail.com>
2019-09-12 22:40:08 +0200

    Update example

--- inst/examples/vancouver-blocks.R
+++ inst/examples/vancouver-blocks.R
@@ -10,6 +10,17 @@ data_url <- paste0(
 blocks <- st_read(data_url) %>%
   geojson_json()
 
-cartovl() %>%
+viz_def <- list(
+  "color: ramp($growth, [red, green, yellow])"
+)
+
+map <- cartovl() %>%
   set_view(latitude = 49.254, longitude = -123.13, zoom = 11) %>%
-  add_layer(blocks, list("color: ramp($growth, [red, green, yellow])"))
+  add_layer(blocks, viz_def)
+
+map <- cartovl() %>%
+  set_view(latitude = 49.254, longitude = -123.13, zoom = 11) %>%
+  add_external_geojson_layer(data_url, viz_def)
+
+
+if (interactive()) map
