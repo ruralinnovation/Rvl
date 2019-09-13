9390d0bb9408a6c50535767505b2697cb0a37bdd
skuethe <crazycapivara@gmail.com>
2019-09-13 08:35:26 +0200

    Add func to make a mapbox text style

--- R/mapboxgl-stuff.R
+++ R/mapboxgl-stuff.R
@@ -19,3 +19,22 @@ add_mapbox_layer <- function(map, source_name, style, id = "mapbox-layer") {
   )
   invoke_method(map, "addMapboxLayer", style)
 }
+
+#' Make a mapbox text style
+#'
+#' @param text_field Value to use for a text label.
+#' @param text_color The color with which the text will be drawn.
+#' @param text-size The size with which the text will be drawn. Units are in pixels.
+#' @export
+mapbox_text_style <- function(text_field, text_color = "white", text_size = 10) {
+  list(
+    type = "symbol",
+    layout = list(
+      "text-field" = sprintf("{%s}", text_field),
+      "text-size" = text_size
+    ),
+    paint = list(
+      "text-color" = "white"
+    )
+  )
+}
--- inst/examples/mapbox-layer_text.R
+++ inst/examples/mapbox-layer_text.R
@@ -16,7 +16,8 @@ map <- cartovl() %>%
   set_view(176.9, -24.655, 2) %>%
   # add_source(data_geojson, "quakes") %>%
   add_source(quakes, "quakes", latitude = "lat", longitude = "long") %>%
-  add_mapbox_layer("quakes", text_style) %>%
+  # add_mapbox_layer("quakes", text_style) %>%
+  add_mapbox_layer("quakes", mapbox_text_style("mag")) %>%
   add_control()
 
 if (interactive()) map
