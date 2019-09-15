b9d6e3e2e7694b7df2dad2661c5a8edc1673a421
skuethe <crazycapivara@gmail.com>
2019-09-15 19:11:48 +0200

    Update example

--- inst/examples/every-person-in-manhatten.R
+++ inst/examples/every-person-in-manhatten.R
@@ -1,5 +1,6 @@
 library(magrittr)
 library(tibble)
+library(cartovl)
 
 data_url <- paste0(
   "https://raw.githubusercontent.com/",
@@ -10,7 +11,10 @@ data_url <- paste0(
 persons <- data_url %>%
   jsonlite::read_json(simplifyVector = TRUE) %>%
   as_tibble() %>%
-  set_names(c("lng", "lat", "sex"))
+  set_names(c("lng", "lat", "sex")) %>%
+  dplyr::mutate(sex = ifelse(sex == 1, "m", "f"))
+
+# persons$sex <- ifelse(persons$sex == 1, "m", "f")
 
 # persons_geojson <- geojsonio::geojson_json(persons)
 
@@ -19,5 +23,5 @@ cartovl() %>%
   add_layer(
     # data = persons_geojson,
     data = persons,
-    viz_def = list("color: ramp($sex, [red, blue])")
+    viz_def = list("color: ramp(buckets($sex, ['m', 'f']), [red, blue])")
   )
