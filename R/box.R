81c694fe5688b867b417f1e87417dccde00fff86
skuethe <crazycapivara@gmail.com>
2019-09-17 19:37:09 +0200

    Fix typo

--- R/box.R
+++ R/box.R
@@ -2,7 +2,7 @@
 #'
 #' @inheritParams add_layer
 #' @param content The text which will be displayed on the map
-#' @param postition position on the map
+#' @param position position on the map
 #' @export
 add_box <- function(map, content, position = "top-right") {
   invoke_method(map, "addBox", content, position)
