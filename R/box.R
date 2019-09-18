3a8a90692893526625763fb1c8de74174f003948
skuethe <crazycapivara@gmail.com>
2019-09-18 22:06:50 +0200

    Add CSS options

--- R/box.R
+++ R/box.R
@@ -1,9 +1,11 @@
+## TODO: Maybe use 'cssText' instead of list
 #' Add a (text) box to the map
 #'
 #' @inheritParams add_layer
-#' @param content The text which will be displayed on the map
-#' @param position position on the map
+#' @param content The text which will be displayed on the map.
+#' @param position The position on the map.
+#' @param ... CSS options applied to the element.
 #' @export
-add_box <- function(map, content, position = "top-right") {
-  invoke_method(map, "addBox", content, position)
+add_box <- function(map, content, position = "top-right", ...) {
+  invoke_method(map, "addBox", content, position, list(...))
 }
--- javascript/src/box.js
+++ javascript/src/box.js
@@ -1,9 +1,10 @@
-export default function(content, position) {
+export default function(content, position, style) {
   const map = this;
   const widgetElement = map.getContainer();
   const boxElement = document.createElement("div");
   // boxElement.className = "map-overlay";
   boxElement.className = `box box-${position || "top-right"}`;
+  Object.assign(boxElement.style, style);
   boxElement.innerHTML = content;
   widgetElement.appendChild(boxElement);
 }
