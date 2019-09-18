e2da87fb51eaf48791896265b564a1b2d53e15eb
skuethe <crazycapivara@gmail.com>
2019-09-18 19:39:13 +0200

    Add 'bearing' and 'pitch' parameters

--- R/map-properties.R
+++ R/map-properties.R
@@ -15,14 +15,15 @@ map_properties <- function(
   )
 }
 
-### TODO: Add 'bearing' and 'pitch'
 #' Set the view of the map
 #'
 #' @inheritParams add_layer
-#' @param longitude longitude
-#' @param latitude latitude
-#' @param zoom zoom
+#' @param longitude The longitude of the geographical centerpoint of the map.
+#' @param latitude The latitude of the  geographical centerpoint of the map.
+#' @param zoom The zoom level of the map.
+#' @param pitch The pitch (tilt) of the map, measured in degrees away from the plane of the screen (0 - 60).
+#' @param bearing The bearing (rotation) of the map, measured in degrees counter-clockwise from north.
 #' @export
-set_view <- function(map, longitude, latitude, zoom = 2) {
-  modify_map_parameters(map, center = c(longitude, latitude), zoom = zoom)
+set_view <- function(map, longitude, latitude, zoom = 2, pitch = 0, bearing = 0) {
+  modify_map_parameters(map, center = c(longitude, latitude), zoom = zoom, pitch = pitch, bearing = bearing)
 }
