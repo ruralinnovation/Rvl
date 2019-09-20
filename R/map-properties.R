3d3ea75b427fb9dfaf0d434a12c206c192af0aa1
skuethe <crazycapivara@gmail.com>
2019-09-20 19:18:27 +0200

    Update r-docs

--- R/map-properties.R
+++ R/map-properties.R
@@ -1,9 +1,11 @@
 ## TODO: rename to 'map_options'!?
 #' Map properties / options
 #'
+#' Properties like \code{center} or \code{zoom} are usually set via \link{set_view}.
+#' The \code{style} can be set by functions like \link{add_carto_basemap} and others.
 #' @param style The mapbox style of the map.
-#' @param min_zoom The minimum zoom level of the map (0-24).
-#' @param max_zoom The maximum zoom level of the map (0-24).
+#' @param min_zoom The minimum zoom level of the map (0 - 24).
+#' @param max_zoom The maximum zoom level of the map (0 - 24).
 #' @param center The inital geographical centerpoint of the map (longitude, latitude coordinate order).
 #' @param zoom The initial zoom level of the map.
 #' @param ... more properties, see \href{https://docs.mapbox.com/mapbox-gl-js/api/}{mabox-gl API reference}
