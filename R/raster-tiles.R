acf98bcda24851de2369ac343f5c8b4b1eaa2ca5
skuethe <crazycapivara@gmail.com>
2019-09-20 18:28:18 +0200

    Update r-docs

--- R/raster-tiles.R
+++ R/raster-tiles.R
@@ -1,8 +1,9 @@
 #' Add raster tiles to the map
 #'
 #' @inheritParams add_layer
-#' @param tiles tile sources
-#' @param attribution attribution
+#' @param tiles A vector of one or more tile source URLs,
+#'   e. g. \code{https://stamen-tiles.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg}
+#' @param attribution The attribution which will be displayed on the map.
 #' @export
 add_raster_tiles <- function(map, tiles = NULL, attribution = NULL) {
   modify_map_parameters(map, rasterStyle = list(tiles = tiles, attribution = attribution))
