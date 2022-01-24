5f493d4218b240de8c8d76c1723bf18909dd20a0
John Hall <john.hall@ruralinnovation.us>
2022-01-24 13:45:35 -0500

    add renv and app example for easy dev start

---
+++ app.R
@@ -0,0 +1,25 @@
+library(sf)
+library(geojsonio)
+library(cartovl)
+
+data_url <- paste0(
+  "https://raw.githubusercontent.com/uber-common/deck.gl-data/",
+  "master/examples/geojson/vancouver-blocks.json"
+)
+
+blocks <- st_read(data_url) %>%
+  geojson_json()
+
+viz_def <- list(
+  "color: ramp($growth, [red, green, yellow])"
+)
+
+map <- cartovl() %>%
+  set_view(latitude = 49.254, longitude = -123.13, zoom = 11) %>%
+  add_layer(blocks, viz_def)
+
+map <- cartovl() %>%
+  set_view(latitude = 49.254, longitude = -123.13, zoom = 11) %>%
+  add_external_geojson_layer(data_url, viz_def)
+
+if (interactive()) map
