8a8f81092edecc010829661994402b45edc624b5
John Hall <john.hall@ruralinnovation.us>
2023-03-30 10:09:52 -0400

    update

--- R/cartovl.R
+++ R/cartovl.R
@@ -39,23 +39,31 @@ cartovl <- function(properties = map_properties(), width = NULL, height = NULL, 
 #' Output and render functions for using cartovl within Shiny
 #' applications and interactive Rmd documents.
 #'
-#' @param outputId output variable to read from
-#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
-#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
-#'   string and have \code{'px'} appended.
-#' @param expr An expression that generates a carto-vl
-#' @param env The environment in which to evaluate \code{expr}.
-#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
-#'   is useful if you want to save an expression in a variable.
-#'
 #' @name rvlOutput
 #' @rdname rvlOutput
+#'
+#' @param outputId output variable to read from
+#' @param width width of div in browser; must be a valid CSS unit (like \code{'100\%'},
+#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
+#'   string and have \code{'px'} appended.
+#' @param height height of div in browser; must be a valid CSS unit (like \code{'100\%'},
+#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
+#'   string and have \code{'px'} appended.
+#' @param minWidth minimum width
+#' @param minHeight minimum height
 #'
 #' @export
 rvlOutput <- function(outputId, width = "100%", height = "100%", minWidth = "480px", minHeight = "360px") {
   htmlwidgets::shinyWidgetOutput(outputId, "cartovl", fill =TRUE, inline = FALSE, width, height, package = "Rvl")
 }
 
+#' Shiny render function
+#'
+#' @param expr An expression that generates a carto-vl
+#' @param env The environment in which to evaluate \code{expr}.
+#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
+#'   is useful if you want to save an expression in a variable.
+#'
 #' @rdname renderVL
 #' @export
 renderVL <- function(expr, env = parent.frame(), quoted = FALSE) {
